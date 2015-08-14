9832b9f22aa1403aa612c46d560eb82d1cbe4faa
menasameh <mena_monster@yahoo.com>
2015-08-14 21:32:51 +0200

    adding first form

--- tripManager/Form1.Designer.cs
+++ tripManager/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.manageTrip = new System.Windows.Forms.Button();
+            this.selectTrip = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // manageTrip
+            // 
+            this.manageTrip.Location = new System.Drawing.Point(47, 81);
+            this.manageTrip.Name = "manageTrip";
+            this.manageTrip.Size = new System.Drawing.Size(75, 23);
+            this.manageTrip.TabIndex = 0;
+            this.manageTrip.Text = "ادارة رحلة";
+            this.manageTrip.UseVisualStyleBackColor = true;
+            // 
+            // selectTrip
+            // 
+            this.selectTrip.Location = new System.Drawing.Point(188, 81);
+            this.selectTrip.Name = "selectTrip";
+            this.selectTrip.Size = new System.Drawing.Size(75, 23);
+            this.selectTrip.TabIndex = 1;
+            this.selectTrip.Text = "اختيار رحلة";
+            this.selectTrip.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(329, 81);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 2;
+            this.exit.Text = "خروج";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(452, 185);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.selectTrip);
+            this.Controls.Add(this.manageTrip);
+            this.Name = "Form1";
+            this.Text = "ادارة الرحلات";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button manageTrip;
+        private System.Windows.Forms.Button selectTrip;
+        private System.Windows.Forms.Button exit;
     }
 }
 
